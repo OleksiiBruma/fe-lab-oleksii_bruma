40e631db1ee3d22071222b50f66cfec9236f17f6
OleksiiBruma <oleksii.bruma@gmail.com>
2019-01-21 02:37:37 +0200

    progress bar added

--- productivity-app/src/app/components/settings/settings.js
+++ productivity-app/src/app/components/settings/settings.js
@@ -34,6 +34,93 @@ var settingsItems = [
     id: "long-break"
   }];
 
+var progressFragment = document.createDocumentFragment();
+var settingsList = document.querySelector(".settings-pomodoro__list");
+var progressBar = document.querySelector(".settings-pomodoro__progress-bar");
+var progress = document.createElement("div");
+progress.classList.add("progress__graph");
+
+function createTopScale() {
+  var topScale = document.createElement("div");
+  topScale.classList.add("progress__top-scale");
+  var startPoint = document.createElement("div");
+  startPoint.classList.add("progress__start-point");
+  startPoint.textContent = "0m";
+  topScale.appendChild(startPoint);
+  var middlePoint = document.createElement("div");
+  middlePoint.classList.add("progress__middle-point");
+  middlePoint.textContent = "First cycle: " + minToHours(getFullCycle()/2);
+  topScale.appendChild(middlePoint);
+  var endPoint = document.createElement("div");
+  endPoint.classList.add("progress__end-point");
+  endPoint.textContent = minToHours(getFullCycle());
+  topScale.appendChild(endPoint);
+  progressFragment.appendChild(topScale);
+}
+
+function createBottomScale (){
+  var bottomScale = document.createElement("div");
+  bottomScale.classList.add("progress__bottom-scale");
+  for (var i=30 ; i<getFullCycle();i+=30) {
+    var bottomScaleItem = document.createElement("div");
+    bottomScaleItem.textContent = minToHours(i);
+    bottomScaleItem.classList.add("progress__bottom-scale-item");
+    bottomScale.appendChild(bottomScaleItem);
+  }
+  progressFragment.appendChild(bottomScale);
+
+}
+
+function createProgress(){
+  while (progress.firstChild) {
+    progress.removeChild(progress.firstChild);
+  }
+createTopScale();
+  createProgressPart();
+  var longBreak = document.createElement("div");
+  longBreak.classList.add("progress__long-break");
+  longBreak.style.width = calcPercentage(settingsItems[3].value);
+  progress.appendChild(longBreak);
+  createProgressPart();
+  progressFragment.appendChild(progress);
+  createBottomScale();
+  while (progressBar.firstChild) {
+    progressBar.removeChild(progressBar.firstChild);
+  }
+  progressBar.appendChild(progressFragment);
+}
+
+function createProgressPart(){
+  for (var i=0; i<settingsItems[1].value; i++){
+    var workTime = document.createElement("div");
+    workTime.classList.add("progress__work-time");
+    workTime.style.width = calcPercentage(settingsItems[0].value);
+    progress.appendChild(workTime);
+    if (i<settingsItems[1].value-1){
+      var shortBreak = document.createElement("div");
+      shortBreak.style.width = calcPercentage(settingsItems[2].value);
+      shortBreak.classList.add("progress__short-break");
+      progress.appendChild(shortBreak);}
+  }
+}
+
+function calcPercentage(number){
+  return ((number/getFullCycle()) * 100).toFixed(2) + "%";
+
+}
+function getFullCycle() {
+  return (settingsItems[0].value + settingsItems[2].value) * settingsItems[1].value - 2 * settingsItems[2].value + settingsItems[3].value;
+}
+
+function minToHours(data) {
+  var minutes = data % 60;
+  var hours = (data - minutes) / 60;
+  if (hours === 0 ) {
+    return minutes.toFixed(0) + "m";
+  }
+
+  return hours + "h"+" "+ minutes.toFixed(0) + "m";
+}
 
 function updateNumberInputes() {
   settingsItems.forEach(function (item) {
@@ -41,40 +128,39 @@ function updateNumberInputes() {
   })
 }
 
-updateNumberInputes();
-
-
-var settingsList = document.querySelector(".settings-pomodoro__list");
-
 function getProgressValue(e) {
-  settingsItems.forEach(function(item){
-    if(e.target.parentElement.children[1].id === item.id ){
-      if(e.target.classList.contains("number__step--up") && +e.target.parentElement.children[1].value < item.max ){
+  settingsItems.forEach(function (item) {
+    if (e.target.parentElement.children[1].id === item.id) {
+      if (e.target.classList.contains("number__step--up") && +e.target.parentElement.children[1].value < item.max) {
         e.target.parentElement.children[0].removeAttribute("disabled");
         e.target.parentElement.children[0].classList.remove("number__step--disabled");
         item.value += item.step;
         updateNumberInputes();
-        if(+e.target.parentElement.children[1].value === item.max ){
-          e.target.parentElement.children[2].setAttribute("disabled","")
+        createProgress();
+        if (+e.target.parentElement.children[1].value === item.max) {
+          e.target.parentElement.children[2].setAttribute("disabled", "");
           e.target.parentElement.children[2].classList.add("number__step--disabled")
-        }}
-
-
-      else if (e.target.classList.contains("number__step--down") && e.target.parentElement.children[1].value > item.min ){
+        }
+      }
+      else if (e.target.classList.contains("number__step--down") && e.target.parentElement.children[1].value > item.min) {
         e.target.parentElement.children[2].removeAttribute("disabled");
         e.target.parentElement.children[2].classList.remove("number__step--disabled");
         item.value -= item.step;
         updateNumberInputes();
-        if(+e.target.parentElement.children[1].value === item.min ){
-          e.target.parentElement.children[0].setAttribute("disabled","")
+        createProgress();
+        if (+e.target.parentElement.children[1].value === item.min) {
+          e.target.parentElement.children[0].setAttribute("disabled", "");
           e.target.parentElement.children[0].classList.add("number__step--disabled")
         }
       }
-    }});
+    }
+  });
+}
 
-  }
 
 
+createProgress();
+updateNumberInputes();
 settingsList.addEventListener("click", getProgressValue);
 
 
