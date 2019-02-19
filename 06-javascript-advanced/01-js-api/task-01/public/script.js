4be5ba6d4e7b132d3f388bc7d8da555d48b001da
Oleksii Bruma <Oleksii_Bruma@epam.com>
2019-02-19 23:03:52 +0200

    js-api task-01 problems with server

--- 06-javascript-advanced/01-js-api/task-01/public/script.js
+++ 06-javascript-advanced/01-js-api/task-01/public/script.js
@@ -1,12 +1,6 @@
 /* JavaScript here */
+
 const dropzone = document.querySelector(".dropzone");
-
-function upload(files) {
-    let fromData = new FormData(),
-        xhr = new XMLHttpRequest(),
-        x;
-
-}
 
 dropzone.ondragover = function (e) {
     e.preventDefault();
@@ -15,12 +9,11 @@ dropzone.ondragover = function (e) {
 dropzone.ondrop = function (e) {
     e.preventDefault();
     this.className = "dropzone dragdrop";
-    //for()
     const files = e.dataTransfer.files;
 
     for (let key in files) {
         if (key === "length") {
-            return
+            break
         }
         if (files[key].type.match(/image/)) {
             while (dropzone.firstChild) {
@@ -39,11 +32,76 @@ dropzone.ondrop = function (e) {
                 dropzone.insertAdjacentHTML("afterbegin", result);
             };
         }
-
     }
+    processFile(e)
 };
 dropzone.ondragleave = function (e) {
     e.preventDefault();
     this.className = "dropzone"
 };
 
+
+function processFile(e) {
+    const files = e.dataTransfer.files;
+    for (let key in files) {
+        if (key === "length") {
+            break
+        }
+        const file = files[key];
+        let start = 0;
+        const name = file.name;
+        const size = file.size;
+        const sliceSize = CHUNK_SIZE;
+        let lastChunk = false;
+        loop(start);
+
+        function loop(start) {
+
+            let end = start + sliceSize;
+            if (size - end < 0) {
+                end = size;
+                lastChunk = true;
+            }
+            let s = slice(file, start, end);
+            send(s, start, end, name, lastChunk);
+        }
+
+        function send(piece, start, end, name, lastChunk) {
+            var formdata = new FormData();
+            var xhr = new XMLHttpRequest();
+            xhr.open('POST', ENDPOINT_URL, true);
+            formdata.append('name', name);
+            formdata.append('start', start);
+            formdata.append('lastChunk', lastChunk);
+            formdata.append('chunk', piece);
+            xhr.send(formdata);
+            xhr.onreadystatechange = () => {
+                if (xhr.readyState === 4) {
+                    if (!lastChunk) {
+                        start = JSON.parse(xhr.responseText).expectedStart;
+                        setTimeout( ()=>loop(start),3 );
+                    }
+
+                }
+            }
+        }
+
+        /**
+         * Formalize file.slice
+         */
+
+        function slice(file, start, end) {
+            var slice = file.mozSlice ? file.mozSlice :
+                file.webkitSlice ? file.webkitSlice :
+                    file.slice ? file.slice : noop;
+
+            return slice.bind(file)(start, end);
+        }
+
+        function noop() {
+
+        }
+
+
+    }
+}
