3409df82fc349cb9e5b53b806f888d8fdf0ad610
Oleksii Bruma <Oleksii_Bruma@epam.com>
2019-02-19 16:13:21 +0200

    js-api task-01 provided visual feedback on Dropzone element when image files are dragged over dropzone (overlay with text), dragged outside or dropped.

--- 06-javascript-advanced/01-js-api/task-01/public/script.js
+++ 06-javascript-advanced/01-js-api/task-01/public/script.js
@@ -1,1 +1,15 @@
 /* JavaScript here */
+const dropzone = document.querySelector(".dropzone");
+dropzone.ondragover = function(e){
+    e.preventDefault();
+    this.className = "dropzone dragover"
+};
+dropzone.ondrop = function(e){
+    e.preventDefault();
+    this.className = "dropzone dragdrop"
+};
+dropzone.ondragleave = function(e){
+    e.preventDefault();
+    this.className = "dropzone"
+};
+
