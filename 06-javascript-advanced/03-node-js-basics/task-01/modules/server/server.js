a3a220bace84228e17f285f77fbd2ef2a5f9bb86
Oleksii Bruma <Oleksii_Bruma@epam.com>
2019-02-25 10:52:51 +0200

    nodejs 2 done

--- 06-javascript-advanced/03-node-js-basics/task-01/modules/server/server.js
+++ 06-javascript-advanced/03-node-js-basics/task-01/modules/server/server.js
@@ -6,10 +6,13 @@ const express = require('express');
 const app = express();
 app.set("views","static");
 app.use("/",express.static("./static/"));
+
 app.get('/', function (req, res) {
     const paths = modules.getFilePaths("./content");
     const categorised = modules.sort(paths);
     res.render('index.pug', {category:categorised});
 });
-
+app.get(/.+/,function(req,res){
+    res.download("content" + req.url)
+});
 app.listen(8000);
