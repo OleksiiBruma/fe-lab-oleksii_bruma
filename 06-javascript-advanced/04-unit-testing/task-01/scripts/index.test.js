3dcfd87b45a0aa17e5ef18f2a00c909418e7736a
OleksiiBruma <oleksii.bruma@gmail.com>
2019-02-27 19:14:08 +0200

    test-3

--- 06-javascript-advanced/04-unit-testing/task-01/scripts/index.test.js
+++ 06-javascript-advanced/04-unit-testing/task-01/scripts/index.test.js
@@ -6,3 +6,11 @@ test("check constructor of the class game",()=>{
   const obj = new Game(20);
   expect(obj.amountOfRounds).toBe(20);
 });
+test("consists of basic properties", ()=>{
+ const obj = new Game(20);
+ expect(obj.amountOfRounds).toBe(20);
+ expect(obj.count).toBe(0);
+ expect(obj.possibilities).toHaveLength(4);
+  expect(obj.currentGame).toHaveLength(0);
+  expect(obj.player).toHaveLength(0)
+});
