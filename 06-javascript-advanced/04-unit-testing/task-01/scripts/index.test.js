e103aaafa04661b241ccd8d4cd80510005679349
OleksiiBruma <oleksii.bruma@gmail.com>
2019-02-27 18:34:56 +0200

    test-2

--- 06-javascript-advanced/04-unit-testing/task-01/scripts/index.test.js
+++ 06-javascript-advanced/04-unit-testing/task-01/scripts/index.test.js
@@ -2,3 +2,7 @@ const Game = require('./index.js');
 test("handle clean game",()=>{
   expect(new Game()).toBeInstanceOf(Game);
 });
+test("check constructor of the class game",()=>{
+  const obj = new Game(20);
+  expect(obj.amountOfRounds).toBe(20);
+});
