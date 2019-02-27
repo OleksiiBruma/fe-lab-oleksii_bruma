c61e3d889a2ed28bd0eb97cfb6e8a04aca9cbeb2
OleksiiBruma <oleksii.bruma@gmail.com>
2019-02-27 19:15:01 +0200

    implementation for test-3

--- 06-javascript-advanced/04-unit-testing/task-01/scripts/index.js
+++ 06-javascript-advanced/04-unit-testing/task-01/scripts/index.js
@@ -1,5 +1,9 @@
 module.exports = class Game{
   constructor(a) {
+    this.count = 0;
+    this.possibilities = ['.button--1', '.button--2', '.button--3', '.button--4'];
+    this.currentGame = [];
+    this.player = [];
     this.amountOfRounds = a;
   }
 };
