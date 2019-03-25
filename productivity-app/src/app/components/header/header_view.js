cc403e4819e2f28b7f31449a8498304bec5e8ba9
Oleksii Bruma <Oleksii_Bruma@epam.com>
2019-03-25 21:20:14 +0200

    phase 2 fixes

--- productivity-app/src/app/components/header/header_view.js
+++ productivity-app/src/app/components/header/header_view.js
@@ -1,10 +1,13 @@
 import {EventBus} from '../../eventBus';
 import template from './header.handlebars';
+
 require('./header.less');
 
 export class Header_view {
   constructor() {
     this.headerTemplate = template;
+    this.makeHeaderSticky = this.makeHeaderSticky.bind(this);
+    this.debounce = this.debounce.bind(this);
   }
 
   init(context) {
@@ -15,32 +18,50 @@ export class Header_view {
     document.querySelector(`[data-id="${id}"]`).classList.add('menu__link--active');
   }
 
+  debounce(func, wait, immediate) {
+    let timeout;
+    return function () {
+      let context = this, args = arguments;
+      const later = function () {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+      let callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
+    };
+  }
 
-  makeHeaderSticky(e) {
-    this.page = document.querySelector('.page');
-    this.headerHTML = document.querySelector('.header');
-    const distance = this.headerHTML.offsetTop + 140 - window.pageYOffset;
-    const offset = window.pageYOffset;
-    this.headerItem = this.headerHTML.querySelector('.header__item');
-    this.headerLogo = this.headerHTML.querySelector('.header__logo');
-    this.addTask = this.headerHTML.querySelector('.page__add-button--header');
-    if (distance <= 0) {
-      if (this.addTask) {
-        this.addTask.classList.remove('hidden');
+  makeHeaderSticky() {
+    this.debounce(function () {
+
+      const page = document.querySelector('.page');
+      const headerHTML = document.querySelector('.header');
+      const distance = headerHTML.offsetTop + 140 - window.pageYOffset;
+      const offset = window.pageYOffset;
+      const headerItem = headerHTML.querySelector('.header__item');
+      const headerLogo = headerHTML.querySelector('.header__logo');
+      const addTask = headerHTML.querySelector('.page__add-button--header');
+      if (distance <= 0) {
+        if (addTask) {
+          addTask.classList.remove('hidden');
+        }
+        headerHTML.classList.remove('header--no-sticky');
+        page.classList.remove('page--no-sticky');
+        headerItem.classList.remove('header__item--no-sticky');
+
+        headerLogo.classList.remove('header__logo--no-sticky');
+      } else if (offset <= headerHTML.offsetTop + 140) {
+        if (addTask) {
+          addTask.classList.add('hidden');
+        }
+        headerHTML.classList.add('header--no-sticky');
+        page.classList.add('page--no-sticky');
+        headerItem.classList.add('header__item--no-sticky');
+        headerLogo.classList.add('header__logo--no-sticky');
       }
-      this.headerHTML.classList.remove('header--no-sticky');
-      this.page.classList.remove('page--no-sticky');
-      this.headerItem.classList.remove('header__item--no-sticky');
-      this.headerLogo.classList.remove('header__logo--no-sticky');
-    } else if (offset <= this.headerHTML.offsetTop + 140) {
-      if (this.addTask) {
-        this.addTask.classList.add('hidden');
-      }
-      this.headerHTML.classList.add('header--no-sticky');
-      this.page.classList.add('page--no-sticky');
-      this.headerItem.classList.add('header__item--no-sticky');
-      this.headerLogo.classList.add('header__logo--no-sticky');
-    }
+    }, 250)();
   }
 
   listenForSticky() {
